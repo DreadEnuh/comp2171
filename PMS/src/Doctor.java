5ccce4352ba2e9d4da1030c17648fd53014707f5
kstar234 <kyle.agws@gmail.com>
2025-03-17 21:01:56 -0500

    Doctor Class updated

--- PMS/src/Doctor.java
+++ PMS/src/Doctor.java
@@ -1,3 +1,24 @@
 public class Doctor {
+    // Attributes
+    private String id;
+    private String firstName;
+    private String middleInitial;
+    private String lastName;
+    private String specialization;
 
+    // Constructor with all fields
+    public Doctor(String id, String firstName, String middleInitial, String lastName, String specialization) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.specialization = specialization;
+    }
+
+    // Constructor without middleInitial (optional)
+    public Doctor(String id, String firstName, String lastName, String specialization) {
+        this(id, firstName, "", lastName, specialization); // Call the main constructor with empty middleInitial
+    }
+
+    //Getters
 }
