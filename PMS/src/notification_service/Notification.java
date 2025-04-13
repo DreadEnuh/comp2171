aa3cddfd1192ff46e62723a8339ac82f6ee7482d
DreadEnuh <Dantethomas.pro@gmail.com>
2025-04-13 18:16:50 -0500

    Update Notification.java

--- PMS/src/notification_service/Notification.java
+++ PMS/src/notification_service/Notification.java
@@ -1,4 +1,36 @@
 package notification_service;
 
 public class Notification {
+    private String recipient;     // email or phone
+    private String subject;       // email subject (can be null for SMS)
+    private String messageBody;
+    private NotificationType type;
+
+    public enum NotificationType {
+        EMAIL,
+        SMS
+    }
+
+    public Notification(String recipient, String subject, String messageBody, NotificationType type) {
+        this.recipient = recipient;
+        this.subject = subject;
+        this.messageBody = messageBody;
+        this.type = type;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getMessageBody() {
+        return messageBody;
+    }
+
+    public NotificationType getType() {
+        return type;
+    }
 }
