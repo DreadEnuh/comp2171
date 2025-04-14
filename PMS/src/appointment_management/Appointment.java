87fb9f0f8c3da7ad3bd4554f58f218471c4bdf09
kstar234 <kyle.agws@gmail.com>
2025-04-14 03:18:07 -0500

    minor push

--- PMS/src/appointment_management/Appointment.java
+++ PMS/src/appointment_management/Appointment.java
@@ -2,7 +2,6 @@ package appointment_management;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.LocalDate;
 
 public class Appointment {
     private String appointmentID, patientID, doctorID;
@@ -25,7 +24,7 @@ public class Appointment {
     }
 
     // Getters
-    public LocalDate getDate() {
+    public LocalDate getAppointmentDate() {
         return date;
     }
 
@@ -75,11 +74,11 @@ public class Appointment {
         this.doctorID = doctorID;
     }
 
-    public LocalDate getDate() {
-        return this.startTime.atDate(LocalDate.now()).toLocalDate();
-    }
+    //public LocalDate getDate() {
+    //     return this.startTime.atDate(LocalDate.now()).toLocalDate();
+    //}
 
     public String getAppointmentSummary() {
-        return "Appointment on " + getDate().toString() + " at " + getStartTime().toString() + " for " + getDuration() + " minutes";
+        return "Appointment on " + getAppointmentDate().toString() + " at " + getStartTime().toString() + " for " + getDuration() + " minutes";
     }
 }
